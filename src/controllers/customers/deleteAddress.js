10e2bc9163bade0d63de2b7c617d89edac54edce
adarsh-anncode <158696907+adarsh-anncode@users.noreply.github.com>
2024-02-15 10:50:07 +0530

    Update deleteAddress.js

--- src/controllers/customers/deleteAddress.js
+++ src/controllers/customers/deleteAddress.js
@@ -3,16 +3,22 @@ import User from "../../models/user.models.js";
 const deleteAddress = async (req, res) => {
     const { id } = req.params
         try {
-        const user = await User.findOne({ id });
-        // if (!user) {
-        //     return res.status(404).json({ message: "User not found" });
-        // }
-        // else
-        //     return res.status(200).json({ message: "Address deleted successfully", data: user })
-        console.log(user, 'main user');
-        delete user["addresses"]
-        console.log(user, 'after delete');
-        console.log(user.addresses, 'after delete');
+        const user = await User.findOneAndUpdate({ id }, 
+            {
+                $unset: { addresses: { } }
+            });
+        if (!user) {
+            return res.status(404).json({ message: "User not found" });
+        }
+        else
+            return res.status(200).json({ message: "Address deleted successfully", data: user })
+        // console.log(user, 'main user');
+        // delete user["addresses"]
+        // console.log(user, 'after delete');
+
+        // user.save();
+        // console.log(user, 'after delete');
+        // console.log(user.addresses, 'after delete');
     } catch (error) {
         console.log(error);
         return res.status(500).json({ message: "Error deleting address" });
